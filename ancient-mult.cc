cdca9f53ba8d23c5a2d4617f1b732f2f63f1fef5
Nasseef Abukamail <abukamai@ohio.edu>
2019-09-19 20:37:08 -0400

    Initial commit

---
+++ ancient-mult.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: ancient-mult.cc
+ *      Author: Author
+ *        Date: Date
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+//function prototypes
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    return 0;
+}// main
